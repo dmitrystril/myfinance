b4edeef45d5d8a770f5e666c754cd25ac58c520b
Dmitry Stril <dmitry.stril@gmail.com>
2019-11-01 01:36:36 +0200

    improvement(dashboard): utilize hooks provided by react-redux

--- client/src/main/component/Dashboard/index.tsx
+++ client/src/main/component/Dashboard/index.tsx
@@ -1,24 +1,17 @@
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux'
 
 import { getTestApiData } from '../../redux/action/TestAction';
 import { AppState } from '../../redux/store';
 import PageContainer from '../common/PageContainer';
 
-interface DashboardProps {
-  testData: string | null,
-  getTestApiData: typeof getTestApiData,
-};
-
-const Dashboard: React.FC<DashboardProps> = (props: DashboardProps) => {
-  const {
-    testData,
-    getTestApiData,
-  } = props;
+const Dashboard: React.FC = () => {
+  const dispatch = useDispatch();
+  const testData = useSelector((state: AppState) => state.testData);
 
   useEffect(() => {
-    getTestApiData();
-  }, [getTestApiData]);
+    dispatch(getTestApiData());
+  }, [dispatch]);
 
   return (
     <PageContainer>
@@ -27,15 +20,4 @@ const Dashboard: React.FC<DashboardProps> = (props: DashboardProps) => {
   );
 };
 
-const mapStateToProps = (state: AppState) => ({
-  testData: state.testData,
-});
-
-const mapDispatchToProps = {
-  getTestApiData,
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Dashboard);
+export default Dashboard;
