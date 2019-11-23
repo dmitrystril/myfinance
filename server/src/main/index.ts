668bc32560d8dd7f0ee2143c64f4c61c9e1d6a4e
Dmitry Stril <dmitry.stril@gmail.com>
2019-11-23 22:46:01 +0200

    refactor(index.ts): remove redundant code

--- server/src/main/index.ts
+++ server/src/main/index.ts
@@ -2,40 +2,25 @@
 import { createConnection } from 'typeorm';
 
 createConnection().then(async connection => {
+  const debug = require('debug')('server:server');
+  const http = require('http');
+
   const app = require('./app').default;
 
-  var debug = require('debug')('server:server');
-  var http = require('http');
-
-  var port = normalizePort(process.env.PORT || '3001');
+  const port = process.env.PORT || 3001;
   app.set('port', port);
 
-  var server = http.createServer(app);
-
+  const server = http.createServer(app);
   server.listen(port);
   server.on('error', onError);
   server.on('listening', onListening);
-
-  function normalizePort(val: any) {
-    var port = parseInt(val, 10);
-
-    if (isNaN(port)) {
-      return val;
-    }
-
-    if (port >= 0) {
-      return port;
-    }
-
-    return false;
-  }
 
   function onError(error: any) {
     if (error.syscall !== 'listen') {
       throw error;
     }
 
-    var bind = typeof port === 'string'
+    const bind = typeof port === 'string'
       ? 'Pipe ' + port
       : 'Port ' + port;
 
@@ -54,8 +39,8 @@ createConnection().then(async connection => {
   }
 
   function onListening() {
-    var addr = server.address();
-    var bind = typeof addr === 'string'
+    const addr = server.address();
+    const bind = typeof addr === 'string'
       ? 'pipe ' + addr
       : 'port ' + addr.port;
     debug('Listening on ' + bind);
